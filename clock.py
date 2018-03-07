702993c78ccca2e34de8d7561148a7ab05148a4c
Boyd Christiansen <boyd.christiansen@gmail.com>
2018-03-07 10:04:18 +0800

    Fixed alphabet test.

--- clock.py
+++ clock.py
@@ -116,7 +116,7 @@ def dict2ref(single_ref_dict):
 	phone = '0' + phone
 	
 	line_lower = LINE.lower()
-	if 'a' not in line_lower or 'b' not in line_lower or 'c' not in line_lower or 'd' not in line_lower or 'e' not in line_lower or 'f' not in line_lower or 'g' not in line_lower or 'h' not in line_lower or 'i' not in line_lower or 'j' not in line_lower or 'k' not in line_lower or 'l' not in line_lower or 'm' not in line_lower or 'n' not in line_lower or 'o' not in line_lower or 'p' not in line_lower or 'q' not in line_lower or 'r' not in line_lower or 's' not in line_lower or 't' not in line_lower or 'u' not in line_lower or 'v' not in line_lower or 'w' not in line_lower or 'x' not in line_lower or 'y' not in line_lower or 'z' not in line_lower:
+	if 'a' not in line_lower and 'b' not in line_lower and 'c' not in line_lower and 'd' not in line_lower and 'e' not in line_lower and 'f' not in line_lower and 'g' not in line_lower and 'h' not in line_lower and 'i' not in line_lower and 'j' not in line_lower and 'k' not in line_lower and 'l' not in line_lower and 'm' not in line_lower and 'n' not in line_lower and 'o' not in line_lower and 'p' not in line_lower and 'q' not in line_lower and 'r' not in line_lower and 's' not in line_lower and 't' not in line_lower and 'u' not in line_lower and 'v' not in line_lower and 'w' not in line_lower and 'x' not in line_lower and 'y' not in line_lower and 'z' not in line_lower:
 		if len(line_lower) == 9:
 			LINE = '0' + LINE
 	#Compile the referal
