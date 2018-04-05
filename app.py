0add3921c80747f6e89f0b9086ae413ee935a82a
Bob Steffen <alexander.steffen@myldsmail.net>
2018-04-05 17:49:21 +0800

    minor str edit

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def webhook():
 											class_time_start = area_row[2]
 											class_time_end = area_row[3]
 											class_address = area_row[4]
-										else:
+										elif area_row[0] != area:
 											send_message(sender_id, "Area not included yet.")
 									class_level_formed = ""
 									if "1" in class_level:
