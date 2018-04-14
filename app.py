58513b6a15e8a963beb4800be8327333c3e99528
Boyd Christiansen <boyd.christiansen@gmail.com>
2018-04-14 09:23:20 +0800

    Removed extra message tag.

--- app.py
+++ app.py
@@ -441,8 +441,7 @@ def send_message(recipient_id, message_text):
 			},
 			"message": {
 				"text": message_text
-			},
-			"tag": "PAIRING_UPDATE"
+			}
 		})
 		r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
